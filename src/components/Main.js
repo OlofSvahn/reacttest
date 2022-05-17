6bdc2499e96545a38107cf07b60e2d27b688478a
Olof Svahn <svahnolof@gmail.com>
2022-05-17 16:29:04 +0200

    init

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>It's a coding structure framework or something</li>
+                <li>Bill Gates did not invent it, nor did Al Gore</li>
+                <li>If you follow the stream down to Fishers Cove you'll find the treasure</li>
+                <li>No one knows where React came from</li>
+                <li>The only way to stop React from taking over the world is with fire</li>
+            </ul>
+        </main>
+    )
+}
